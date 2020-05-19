d9dfccca7bc4e0ea06fabc3ed9d9ef1a106da530
yuerpustc@gmail.com <yuerpustc@gmail.com>
2020-05-20 00:37:02 +0800

    ground_truth

---
+++ Plot_GroundTruth.m
@@ -0,0 +1,45 @@
+%%绘制Indian Pines 地面真值
+% 加载数据
+data_name='Indian_pines_corrected.mat';
+data_gt='Indian_pines_gt.mat';
+addpath(genpath('utils'))
+addpath(genpath('dataset'))
+a=load(data_name);
+Data = a.data;
+[row,col,num_feature] = size(Data);
+a=load(data_gt);%ground truth
+Label = reshape(double(a.groundT),row*col,1);
+clear a;
+X_result = drawresult(Label,row,col, 2);
+
+%输出真值图像
+str0='C:\Documents\graduation_project\HSIC_RPNet\figure\';
+str1='Indian_Pines_GroundTruth';
+str2='.png';
+save_path=[str0,str1,str2];
+imwrite(X_result,save_path);
+%%
+
+
+%%绘制Indian Pines 地面真值
+% 加载数据
+data_name='PaviaU.mat';
+data_gt='PaviaU_gt.mat';
+addpath(genpath('utils'))
+addpath(genpath('dataset'))
+a=load(data_name);
+Data = a.paviaU;
+[row,col,num_feature] = size(Data);
+a=load(data_gt);
+Label = reshape(double(a.paviaU_gt),row*col,1);
+clear a;
+X_result = drawresult(Label,row,col, 2);
+
+%输出真值图像
+str0='C:\Documents\graduation_project\HSIC_RPNet\figure\';
+str1='PaviaU_GroundTruth';
+str2='.png';
+save_path=[str0,str1,str2];
+imwrite(X_result,save_path);
+%%
+
